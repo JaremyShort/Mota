7318bdbcd3c555b7ac163052c3e606c65d187fdc
Jaremy <475532514@qq.com>
2026-04-25 16:01:24 +0800

    Revert "魔塔小游戏"
    This reverts commit 33eaa1fb5c4c42b84f8c7e69884972df0f3062ea.

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
-  testMatch: ['**/tests/**/*.test.js'],
-  moduleDirectories: ['js', 'tests'],
-  verbose: true
-};
